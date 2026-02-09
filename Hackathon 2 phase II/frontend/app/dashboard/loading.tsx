e66472b2f09db1ef0fca6f0db3ca230ca5cd77c6
Qasim Hussain <qasim.hussain2410@gmail.com>
2026-02-09 23:01:55 +0500

    Delete Hackathon 2 phase II directory

--- Hackathon 2 phase II/frontend/app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from '@/components/ui/spinner';
-
-export default function DashboardLoading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Spinner label="Loading dashboard..." />
-    </div>
-  );
-}
